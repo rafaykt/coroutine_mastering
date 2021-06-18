614923d6e145668eae838d579e2bcfa7430e2ea0
Rafael <rafael.ito92@hotmail.com>
2021-06-18 16:48:36 -0300

    usecase 6 - Retry with callback

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,45 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfTries = 2
+
+            try {
+                retry(numberOfTries ){
+                    loadRecentAndroidVersion()
+                }
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+        }
+    }
+
+    private suspend fun <T> retry(numberOfRetries: Int, block: suspend () -> T):T {
+        repeat(numberOfRetries) {
+            try {
+                return  block()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+            }
+        }
+        return block()
+    }
+
+    private suspend fun loadRecentAndroidVersion() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
