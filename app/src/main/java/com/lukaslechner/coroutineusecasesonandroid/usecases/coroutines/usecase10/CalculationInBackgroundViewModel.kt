ea4d18ca5b6a37aa910f32b5aa5d858c526d16d3
Rafael <rafael.ito92@hotmail.com>
2021-06-21 16:54:09 -0300

    Coroutine Context - usecase 10

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase10/CalculationInBackgroundViewModel.kt
@@ -1,10 +1,37 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase10
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CalculationInBackgroundViewModel : BaseViewModel<UiState>() {
 
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            var result = BigInteger.ONE
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorial(factorialOf)
+            }
+            var resultToString = ""
+            val toStringDuration = measureTimeMillis {
+                resultToString = withContext(Dispatchers.IO) { result.toString() }
+            }
 
+            uiState.value = UiState.Success(resultToString, computationDuration, toStringDuration)
+        }
+    }
+
+    private suspend fun calculateFactorial(number: Int) = withContext(Dispatchers.IO) {
+        var factorialResult = BigInteger.ONE
+        for (iteradorFatorial in 1..number) {
+            factorialResult =
+                factorialResult.multiply(BigInteger.valueOf(iteradorFatorial.toLong()))
+        }
+        factorialResult
     }
 }
