9c6d63c59451a016cf096eec2360f1409567f2d0
Rafael <rafael.ito92@hotmail.com>
2021-06-22 16:05:02 -0300

    S09E05 - parte 2 - não recomendado dar overwrite em jobs

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structured_concurrency/4_cancelling_parents_and_childs.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/structured_concurrency/4_cancelling_parents_and_childs.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.*
 
 fun main() {
 
-    val scopeJob = Job()
+    /*val scopeJob = Job()
     val scope = CoroutineScope(Dispatchers.Default + scopeJob)
 
     var childCoroutineJob: Job? = null
@@ -21,7 +21,26 @@ fun main() {
 
     println("Is childCoroutineJob a child of coroutineJob? ---> ${coroutineJob.children.contains(childCoroutineJob)}")
 
-    println("is coroutinejob a child of scopejob? ---> ${scopeJob.children.contains(coroutineJob)}")
+    println("is coroutinejob a child of scopejob? ---> ${scopeJob.children.contains(coroutineJob)}")*/
+/**
+ * O que será feito abaixo não é recomendado. É apenas demonstrativo.
+ * */
+    val scopeJob = Job()
+    val scope = CoroutineScope(Dispatchers.Default+ scopeJob)
+
+    val passedJob = Job()
+    val coroutineJob = scope.launch(passedJob){
+        println("Starting coroutine")
+        delay(100)
+    }
+
+    Thread.sleep(100)
+
+    println("passedJob and coroutineJob are references to the same job object? ---> ${passedJob===coroutineJob}")
+
+    println("Is coroutineJob a child of scopeJob? ---> ${scopeJob.children.contains(coroutineJob)}")
+
+
 
 }
 
