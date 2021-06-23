66183e7fc7c5f8f756b97831e076648fbc6e10f3
Rafael <rafael.ito92@hotmail.com>
2021-06-23 10:07:45 -0300

    Cooperative cancellation

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/cancellation/2_cooperative_cancelation.kt
@@ -0,0 +1,24 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.cancellation
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    val job = launch(Dispatchers.Default) {
+        repeat(10) { index ->
+            /*yield()
+            println("Operation number $index")
+            Thread.sleep(100)*/
+            if(isActive) {
+                println("Operation number $index")
+                Thread.sleep(100)
+            }else{
+                println("Cancelling...")
+                throw CancellationException()
+            }
+        }
+    }
+    delay(250)
+    println("Cancelling Coroutine")
+    job.cancel()
+}
