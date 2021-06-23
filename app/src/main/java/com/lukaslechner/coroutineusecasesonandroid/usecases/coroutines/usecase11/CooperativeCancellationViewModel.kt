b9f7f818e238466ee83c60c7cdc68e9b7d93f4cb
Rafael <rafael.ito92@hotmail.com>
2021-06-23 10:17:44 -0300

    Cancellation in usecase 10

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase11/CooperativeCancellationViewModel.kt
@@ -3,19 +3,50 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import timber.log.Timber
+import java.math.BigInteger
+import kotlin.system.measureTimeMillis
 
 class CooperativeCancellationViewModel(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : ViewModel() {
 
+    var calculationJob: Job()? = null
     fun performCalculation(factorialOf: Int) {
+        uiState.value = UiState.Loading
+        calculationJob = viewModelScope.launch {
+            var result = BigInteger.ONE
+            val computationDuration = measureTimeMillis {
+                result = calculateFactorial(factorialOf)
+            }
+            var resultToString = ""
+            val toStringDuration = measureTimeMillis {
+                resultToString = withContext(Dispatchers.IO) { result.toString() }
+            }
 
+            uiState.value = UiState.Success(resultToString,computationDuration, toStringDuration)
+        }
+        calculationJob?.invokeOnCompletion { throwable ->
+            if(throwable is CancellationException){
+              Timber.d ("Calculation was cancelled!!")
+            }
+        }
+    }
+
+    private suspend fun calculateFactorial(number: Int) = withContext(Dispatchers.IO) {
+        var factorialResult = BigInteger.ONE
+        for (iteradorFatorial in 1..number) {
+            yield()
+            factorialResult =
+                factorialResult.multiply(BigInteger.valueOf(iteradorFatorial.toLong()))
+        }
+        factorialResult
     }
 
     fun cancelCalculation() {
-
+        calculationJob?.cancel
     }
 
     fun uiState(): LiveData<UiState> = uiState
