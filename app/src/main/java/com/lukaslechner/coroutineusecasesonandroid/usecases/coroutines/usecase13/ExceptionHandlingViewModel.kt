dd51eb77d03b60c16b854c1cba607854ee3e74fb
Rafael <rafael.ito92@hotmail.com>
2021-06-23 18:06:10 -0300

    Exception handling usecase 13

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase13/ExceptionHandlingViewModel.kt
@@ -1,8 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase13
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
 import kotlinx.coroutines.*
+import retrofit2.HttpException
 import timber.log.Timber
 
 class ExceptionHandlingViewModel(
@@ -10,14 +12,63 @@ class ExceptionHandlingViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun handleExceptionWithTryCatch() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                api.getAndroidVersionFeatures(27)
+            } catch (exc: Exception) {
+                if (exc is HttpException) {
+                    if (exc.code() == 500) {
+                        uiState.value = UiState.Error("Erro 500 $exc")
+                    } else {
+                        uiState.value = UiState.Error("Erro que não é o 500 $exc")
+                    }
+                }
+            }
+        }
     }
 
     fun handleWithCoroutineExceptionHandler() {
+        uiState.value = UiState.Loading
+        val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+            uiState.value = UiState.Error("Erro em : $throwable")
+        }
+
+        viewModelScope.launch(exceptionHandler) {
+            api.getAndroidVersionFeatures(27)
+        }
 
     }
 
     fun showResultsEvenIfChildCoroutineFails() {
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            supervisorScope {
+                val oreoFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(27)
+                }
+                val pieFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(28)
+                }
+                val qFeaturesDeferred = async {
+                    api.getAndroidVersionFeatures(29)
+                }
 
+                val versionFeatures = listOf(
+                    oreoFeaturesDeferred,
+                    pieFeaturesDeferred,
+                    qFeaturesDeferred
+                ).mapNotNull {
+                    try {
+                        it.await()
+                    } catch (exception: Exception) {
+                        Timber.e("Error loading Feature data")
+                        null
+                    }
+                }
+
+                uiState.value = UiState.Success(versionFeatures)
+            }
+        }
     }
 }
